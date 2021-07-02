62e8c4092cbf69439c4d23f875f40a71bd23c400
arpangreat <arpanthegreat41@gmail.com>
2021-07-02 21:59:17 +0530

    clean ups

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -37,6 +37,7 @@ use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
+    hosting::seat_at_table();
 
     // Absoulute Path
     crate::front_of_house::hosting::add_to_waitlist();
