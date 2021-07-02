26cefefde59569ea981ad95d92245cb5f9814f40
arpangreat <arpanthegreat41@gmail.com>
2021-07-02 21:57:02 +0530

    Paths with use

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -33,7 +33,11 @@ mod front_of_house {
     }
 }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+
     // Absoulute Path
     crate::front_of_house::hosting::add_to_waitlist();
 
