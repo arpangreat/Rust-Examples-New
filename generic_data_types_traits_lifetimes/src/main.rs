505a5d4b576e8a43c0141d631e3ee3b22f747fbb
arpangreat <arpanthegreat41@gmail.com>
2021-08-06 21:27:48 +0530

    generic data types, traits

---
+++ generic_data_types_traits_lifetimes/src/main.rs
@@ -0,0 +1,45 @@
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_chars(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_in_traits<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 60, 70, 88, 123];
+
+    println!("The largest number is: {}", largest(&number_list));
+
+    let chars = vec!['a', 'c', 'y', 'm'];
+
+    println!("The largest char is: {}", largest_chars(&chars));
+}
