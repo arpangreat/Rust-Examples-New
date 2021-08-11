dc39e38200bf433ef0187c17974ec6134aecfa54
arpangreat <arpanthegreat41@gmail.com>
2021-08-11 20:06:03 +0530

    generic data types

--- generic_data_types_traits_lifetimes/src/main.rs
+++ generic_data_types_traits_lifetimes/src/main.rs
@@ -22,17 +22,17 @@ fn largest_chars(list: &[char]) -> char {
     largest
 }
 
-fn largest_in_traits<T>(list: &[T]) -> T {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
+// fn largest_in_traits<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+//
+//     for &item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//
+//     largest
+// }
 
 fn main() {
     let number_list = vec![34, 50, 60, 70, 88, 123];
---
+++ generic_data_types_traits_lifetimes/src/traits.rs
@@ -0,0 +1,18 @@
+pub fn run() {}
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct Newspaper {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for Newspaper {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
