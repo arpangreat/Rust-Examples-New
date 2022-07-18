5f184b65d8123e8ac6390b446f4f0ac356cad01d
arpangreat <arpanthegreat41@gmail.com>
2022-07-19 04:37:46 +0530

    clean ups

--- control_flow_demo/target/debug/control_flow_demo.d
+++ control_flow_demo/target/debug/control_flow_demo.d
@@ -1,1 +1,1 @@
-/home/arpangreat/Rust-Examples-New/control_flow_demo/target/debug/control_flow_demo: /home/arpangreat/Rust-Examples-New/control_flow_demo/src/main.rs
+/home/arpangreat/rust-examples-new/control_flow_demo/target/debug/control_flow_demo: /home/arpangreat/rust-examples-new/control_flow_demo/src/main.rs
